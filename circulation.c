e31bd83dc5f9db1c95c4f1081f73f1a4fa3ca8f9
林政浩 <1392872316@qq.com>
2023-03-16 21:24:32 +0800

    添加项目文件。

---
+++ circulation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+//int main(void) {
+//	double sum1, sum2, sum3;
+//	int i, j;
+//	sum1 = sum2 = sum3 = 0;
+//
+//	for (i = 1, j = 1; (double)1 / i > pow(10, -4); i += 2, j++) {
+//		if (j == 1 || j % 2 != 0)
+//			sum1 += (double)1 / i;
+//		else
+//			sum2 += (double)1 / i;
+//	}
+//	sum3 = sum1 - sum2;
+//	printf("%g", sum3 * 4);
+//
+//	return 0;
+//}
+
+int main(void) {
+
+	return 0;
+}
