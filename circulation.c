d1eeb1f013515ce79caa9b1deedc71bb4af6a0c7
林政浩 <1392872316@qq.com>
2023-03-17 21:49:39 +0800

    0317

--- circulation.c
+++ circulation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <math.h>
 
 //int main(void) {
@@ -18,7 +19,39 @@
 //	return 0;
 //}
 
+//int main(void) {
+//	int A, B, C, D, i, status[5];
+//	char max;
+//
+//	A = 1 - 1;
+//	B = 1 + 1;
+//	C = 1 + 1 - 1;
+//	D = 1;
+//
+//	max = A < B ? (B < C ? (C < D ? D:C): (max = B < D ? D : B)): (A < C ? (C < D ? D : C) : (A < D ? D : A));
+//	if (max == A)
+//		printf("A最有嫌疑。");
+//	else if(max == B)
+//		printf("B最有嫌疑。");
+//	else if(max == C)
+//		printf("C最有嫌疑。");
+//	else if(max == D)
+//		printf("D最有嫌疑。");
+//
+//	return 0;
+//}
+
 int main(void) {
+	int num1, num2, i, min, common_divisor;
+
+	printf("输入两个整数：");
+	scanf_s("%d %d", &num1, &num2);
+	min = num1 < num2 ? num1 : num2;
+	for (i = 1; i <= min; i++) {
+		if (num1 % i == 0 && num2 % i == 0)
+			common_divisor = i;
+	}
+	printf("最大公约数为：%d", common_divisor);
 
 	return 0;
 }
